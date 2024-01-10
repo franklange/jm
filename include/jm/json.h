707f7abbd7b342d78a5efb8c7be116a34faaa6f0
Frank Lange <langefrq@gmx.de>
2024-01-10 14:50:06 +0100

    add json pretty print

--- include/jm/json.h
+++ include/jm/json.h
@@ -9,6 +9,7 @@ namespace jm {
 
 using Json = nlohmann::json;
 
+void pprint(const Json&);
 auto to_string(const Json&, int indent = 2) -> std::string;
 
 } // namespace jm
--- src/json.cpp
+++ src/json.cpp
@@ -1,6 +1,13 @@
 #include <jm/json.h>
 
+#include <iostream>
+
 namespace jm {
+
+void pprint(const Json& j)
+{
+    std::cout << j.dump(2) << std::endl;
+}
 
 auto to_string(const Json& j, const int indent) -> std::string
 {
